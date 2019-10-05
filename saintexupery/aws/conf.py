096c9a23e6f9980cbf8653951268d587cdc47f71
excellencemichel <bnvnmmnl@gmail.com>
2019-10-05 01:58:45 +0100

    Fixe CDN

--- saintexupery/aws/conf.py
+++ saintexupery/aws/conf.py
@@ -11,7 +11,7 @@ from saintexupery.production import  AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 
 AWS_STORAGE_BUCKET_NAME = 'saintexupery'
-AWS_S3_ENDPOINT_URL = 'https://saintexupery.fra1.digitaloceanspaces.com'
+AWS_S3_ENDPOINT_URL = 'https://saintexupery.fra1.digitaloceanspaces.com/'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
@@ -20,16 +20,14 @@ AWS_STATIC_LOCATION = 'static/'
 
 AWS_MEDIA_LOCATION = 'media/'
 
-STATIC_URL = "{aws_s3_endpoint_url}/{aws_static_location}".format( aws_s3_endpoint_url= AWS_S3_ENDPOINT_URL, aws_static_location= AWS_STATIC_LOCATION)
-MEDIA_URL = "{aws_s3_endpoint_url}/{aws_media_location}".format(aws_s3_endpoint_url=AWS_S3_ENDPOINT_URL, aws_media_location= AWS_MEDIA_LOCATION)
+STATIC_URL = AWS_S3_ENDPOINT_URL + 'static/' 
+MEDIA_URL =  AWS_S3_ENDPOINT_URL + 'media/'
 MEDIA_ROOT = MEDIA_URL
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 
 DEFAULT_FILE_STORAGE = 'saintexupery.aws.utils.MediaRootS3BotoStorage'
-
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'saintexupery.aws.utils.StaticRootS3BotoStorage'
+STATICFILES_STORAGE = 'saintexupery.aws.utils.StaticRootS3BotoStorage'
 
 
 
