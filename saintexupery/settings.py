183ad40d6384008fe215ab4d6a6af9f5dcf189ee
excellencemichel <bnvnmmnl@gmail.com>
2019-11-04 23:18:53 +0100

    Fixed setting

--- saintexupery/settings.py
+++ saintexupery/settings.py
@@ -196,7 +196,6 @@ STATICFILES_DIRS = [
                 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
 
-MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
 
