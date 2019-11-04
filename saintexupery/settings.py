ed5482965a9e6e90c447553352213231da1aef55
excellencemichel <bnvnmmnl@gmail.com>
2019-11-04 22:21:50 +0100

    Aranngement du cvs

--- saintexupery/settings.py
+++ saintexupery/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = []
 BASE_URL ="www.centrecnedsaintexupery.com"
 
 
