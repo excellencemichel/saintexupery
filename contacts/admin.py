cd4a7a23999a63b85d5f904b6ab362bfc05634d9
excellencemichel <bnvnmmnl@gmail.com>
2020-05-26 17:50:34 +0000

    Ajouter filtre contact

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,4 +4,9 @@ from .models import Contact
 # Register your models here.
 
 
-admin.site.register(Contact)
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+	list_filter = ("created", "created",) #Pour les champs qui vont s'afficher dans l'admin
+
+
+
